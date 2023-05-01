8120798587bdde47cfe753b7cfe994792806eb79
KemoniCodes <kemoniw333@gmail.com>
2023-05-01 02:20:07 -0700

    global styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "dark-cocoa": "var(--darkcocoa)",
+        "powder-iris": "var(--powderiris)",
+        "light-creme": "var(--lightcreme)",
       },
     },
   },
