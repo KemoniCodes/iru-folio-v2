7eefd1960ec1cfecbbacf7541c802441a0136efa
KemoniCodes <kemoniw333@gmail.com>
2023-05-01 02:40:56 -0700

    initial navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 export default function HomePage() {
     return (
         <>
-            <header>
+            <header className="w-full">
                 <Navbar />
             </header>
         </>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,28 @@
+import Link from "next/link"
+import Image from "next/image"
+import Logo from '@/public/logo.png'
+
+
 export default function Navbar() {
     return (
         <>
+            <nav className="z-[1] relative">
+                <ul className=" flex list-none justify-between m-4">
+                    <li className='nav-link'>
+                        <Link href={'/menu'}>menu</Link>
+                    </li>
+                    <li className='nav-link'>
+                        <Link href={'/'}>
+                            <Image src={Logo} alt='Iru Studios Logo' width={130}/>
+                        </Link>
+                    </li>
+                    <li className='nav-link'>
+                        <Link className='nav-link' href={'/contact'}>
+                            contact
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
         </>
     )
 }
