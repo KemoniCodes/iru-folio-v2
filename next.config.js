94d1e96772a4a3dea92a607b2a886d39caedff26
KemoniCodes <kemoniw333@gmail.com>
2023-05-15 23:32:10 -0700

    tagline animations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'media3.giphy.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media2.giphy.com',
+        port: '',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
