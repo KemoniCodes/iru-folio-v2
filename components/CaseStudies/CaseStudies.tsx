27e616d22bfcfb1f201a7c84f065a2bd385f9949
KemoniCodes <kemoniw333@gmail.com>
2023-05-20 15:59:42 -0700

    case study listing page

--- components/CaseStudies/CaseStudies.tsx
+++ components/CaseStudies/CaseStudies.tsx
@@ -1,21 +1,44 @@
-import React from "react";
+import React, { useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { CaseStudiesProps } from "./CaseStudiesProps";
+import { motion, useInView, useScroll, useTransform } from "framer-motion";
+
 
 function Case({ title, thumbnail, services }: CaseStudiesProps) {
+    const ref = useRef(null);
+    const isInView = useInView(ref, { once: false });
+
+    let lastItem = services ? services[services.length - 1] : null;
+
     return (
-        <Link href={`/works/${title}`} key={title}>
-            {thumbnail && (
-                <Image src={thumbnail} width={200} height={200} alt="thumbnail" />
-            )}
-            <h3>{title}</h3>
-            <ul>
-                {services?.map((service, index) => (
-                    <li key={index}>{service}</li>
-                ))}
-            </ul>
-        </Link>
+        <motion.div
+            ref={ref}
+            className="caseStudy justify-center flex w-full items-center pb-14 even:justify-end"
+        >
+            <Link
+                href={`/works/${title}`}
+                key={title}
+                style={{
+                    transform: isInView ? "none" : "translateY(-80px)",
+                    opacity: isInView ? 1 : 0,
+                    transition:
+                        "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1.1) 0.5s",
+                }}
+            >
+                {thumbnail && <Image src={thumbnail} width={300} height={300} alt="thumbnail" />}
+                <div className="info flex justify-between items-center">
+                    <h4 className="py-2">{title}</h4>
+                    <ul className="flex gap-1">
+                        {services?.map((service, index) => (
+                            <li key={index}>
+                                <h4>{lastItem === service ? service : `${service}/`}</h4>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </Link>
+        </motion.div>
     );
 }
 
@@ -25,20 +48,25 @@ const caseStudyData: CaseStudiesProps[] = [
         title: "sera",
         services: ["branding", "web design"],
     },
-    // {
-    //     thumbnail: "/soot-hover.png",
-    //     title: "SOOT",
-    //     services: ["branding", "web design"],
-    // },
+    {
+        thumbnail: "/soot-hover.png",
+        title: "SOOT",
+        services: ["branding", "web design"],
+    }
 ];
 
 export default function CaseStudies() {
     return (
         <>
-            <h1>CASE STUDIES PAGE</h1>
-            {caseStudyData.map((caseData, index) => (
-                <Case key={index} {...caseData} />
-            ))}
+            <div className="title text-center fixed">
+                <h2>RECENT <br />/WORKS</h2>
+            </div>
+            <div className="caseStudies px-16">
+                {caseStudyData.map((caseData, index) => (
+                    <Case key={index} {...caseData} />
+                ))}
+            </div>
         </>
     );
 }
+
--- pages/works.js
+++ pages/works.js
@@ -4,7 +4,7 @@ import CaseStudies from '@/components/CaseStudies/CaseStudies';
 export default function CaseStudiesPage() {
     return (
         <>
-            <div className="case-studies w-full">
+            <div className="case-studies w-full pb-16 mt-16">
                 <CaseStudies />
             </div>
         </>
