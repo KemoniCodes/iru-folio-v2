177a1e3a1f3589eb2f421dd859809ac3f5397ee1
KemoniCodes <kemoniw333@gmail.com>
2023-05-21 02:27:29 -0700

    build fix

--- components/About/Hero.tsx
+++ components/About/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
         <>
             <div className="hero border-b-solid border-b-[3px] border-b-dark-cocoa pb-12">
                 <h2>iru studios is an independent design studio based in OC, CA. Founded by creative developer, <em>Kemoni</em>.</h2>
-                <h3 className="py-4 font-normal">Here we &lt;3 less is more. <br /> Intentional design that's <br />made to feel.</h3>
+                <h3 className="py-4 font-normal">Here we &lt;3 less is more. <br /> Intentional design that&apos;s <br />made to feel.</h3>
             </div>
         </>
     )
