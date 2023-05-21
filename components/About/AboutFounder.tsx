42fc3d24a7a2e9015661e7bb4dd8a0c398b9fe53
KemoniCodes <kemoniw333@gmail.com>
2023-05-21 02:42:01 -0700

    mobile

--- components/About/AboutFounder.tsx
+++ components/About/AboutFounder.tsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function AboutFounder() {
     return (
         <>
-            <div className="aboutFounder flex py-16 border-b-solid border-b-[3px] border-b-dark-cocoa">
-                <h3 className="font-normal w-[40%]">about founder</h3>
-                <div className="aboutMe w-[85%]">
-                    <p className="w-[60%]">
+            <div className="aboutFounder lg:flex block py-16 border-b-solid border-b-[3px] border-b-dark-cocoa lg:mb-0 mb-64">
+                <h3 className="font-normal lg:w-[40%] w-full">about founder</h3>
+                <div className="aboutMe lg:w-[85%] w-full">
+                    <p className="lg:w-[60%] w-full">
                         A DEVELOPER TURNED DESIGNER. GETTING MY START 
                         IN PROGRAMMING, I LOVE TO UTILIZE BOTH MY LEFT 
                         AND RIGHT BRAIN.<br/><br/> INFUSING LOGIC AND CREATIVITY, 
