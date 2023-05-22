048f436993ed27284251817c074f0062410b2281
KemoniCodes <kemoniw333@gmail.com>
2023-05-21 20:37:46 -0700

    pinterest tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
     <Html lang="en" className='mb-4 mx-8'>
       <Head>
         <link rel='shortcut icon' sizes="196x196" href='/IRU-CIRCLE.svg'/>
+        <meta name="p:domain_verify" content="dd21365f117c2771a52685cbcce242a1"/>
       </Head>
       <body>
         <Main />
