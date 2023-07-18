1d9fb91b7b093ff19ffafb5b3195af02567d6599
KemoniCodes <kemoniw333@gmail.com>
2023-07-18 02:14:03 -0700

    mobile fix

--- pages/shop/product/[slug].js
+++ pages/shop/product/[slug].js
@@ -3,7 +3,7 @@ import PDP from '../../../components/Shop/Products/PDP/PDP';
 export default function ProductPage() {
     return (
         <>
-            <div className="product-page h-screen w-full mb-40 mt-16">
+            <div className="product-page lg:h-screen h-auto w-full mb-40 mt-16">
                 <PDP />
             </div>
         </>
